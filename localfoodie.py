5ea69b182688c2edff697a81bbb0ea651913ff56
ctran30 <54650720+ctran30@users.noreply.github.com>
2019-09-25 17:36:57 -0700

    Update localfoodie.py

--- localfoodie.py
+++ localfoodie.py
@@ -6,25 +6,29 @@ app.config['SECRET_KEY'] = 'some-key'
 
 @app.route('/home')
 def home():
-
+    return render_template('home.html')
 
 
 @app.route('/login')
 def login():
+    return render_template('login.html')
 
 
 
 @app.route('/category-choices')
 def choices():
+    return render_template('choices.html')
 
 
 @app.route('/list-of-restaurants')
 def restaurants():
+    return render_template('restaurants.html')
 
 
 
 @app.route('/checkout')
 def checkout():
+    return render_template('checkout.html')
 
 
 
