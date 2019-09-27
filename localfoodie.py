a5fa5ecf4d0f37e87fddfa12a9c2375d1fb309cc
ctran30 <54650720+ctran30@users.noreply.github.com>
2019-09-27 12:08:00 -0700

    Update localfoodie.py

--- localfoodie.py
+++ localfoodie.py
@@ -1,36 +1,26 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'some-key'
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/login')
+@app.route('/')
+def index():
+    list_of_steps = ['1. Set your location', '2. Select your category of food',
+        '3. Select your restaurant', '4. Select your food', '5. Order and pay']
+    return render_template('index.html', list_of_steps=list_of_steps)
+    
 def login():
     return render_template('login.html')
 
+def categories():
+    return render_template('categories.html')
 
-
-@app.route('/category-choices')
-def choices():
-    return render_template('choices.html')
-
-
-@app.route('/list-of-restaurants')
 def restaurants():
     return render_template('restaurants.html')
 
+def selection():   
+    return render_template('selection.html')
 
-
-@app.route('/checkout')
 def checkout():
     return render_template('checkout.html')
-
-
 
 if __name__ == '__main__':
     app.run();
