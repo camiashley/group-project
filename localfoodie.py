48778b5fb24e0e5d08f4501d7daf49b7b4a8d81b
ctran30 <54650720+ctran30@users.noreply.github.com>
2019-09-28 23:44:26 -0700

    Update localfoodie.py

--- localfoodie.py
+++ localfoodie.py
@@ -7,16 +7,28 @@ def index():
         '3. Select your restaurant', '4. Select your food', '5. Order and pay']
     return render_template('index.html', list_of_steps=list_of_steps)
     
-def login():
-    return render_template('login.html')
+@app.route('/settings', methods = ['GET', 'POST'])
+def settings():
+    username = request.form.get('username')
+    return render_template('settings.html', username=username)
 
+@app.route('/categories', methods = ['GET', 'POST'])
 def categories():
-    return render_template('categories.html')
+    location = request.form.get('location')
+    radius = request.form.get('radius')
+    return render_template('categories.html', location=location, radius=radius)
 
+@app.route('/restaurants', methods = ['GET', 'POST'])
 def restaurants():
-    return render_template('restaurants.html')
+    campus_food = ['• Panda Express', '• Subway', '• Jamba Juice', "• Steak n' Shake", '• Taco Bell', '• Build Pizza', '• Japanese Fusion', '• Paseo Fresh', '• Starbucks']
+    viet = ['• VietNoms', '• Banh Mi Oven', "• Lee's Sandwiches", '• Pho Passion']
+    chinese = ['• Panda Express', '• China Chen']
+    mex = ['• Taco Bell', '• La Vics', '• Iguanas', '• Chipotle']
+    fastfood = ['• Taco Bell', '• Jack in the Box', '• McDonalds', '• Chipotle']
+    quick = ['• Gongcha', '• Breaktime', '• Village Market', '• 7Eleven']
+    return render_template('restaurants.html', campus_food=campus_food, viet=viet, chinese=chinese, mex=mex, fastfood=fastfood, quick=quick)
 
-def selection():   
+def selection():
     return render_template('selection.html')
 
 def checkout():
