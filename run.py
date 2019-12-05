c5d8056ca2216f12165f4c60c373d8f6f326329f
Hoangle95 <54647282+Hoangle95@users.noreply.github.com>
2019-12-04 16:57:26 -0800

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from application import app, db
-from application.models import User, Post
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db':db, 'User':User, 'Post': Post}
-
-if __name__ == "__main__":
-    app.run()
